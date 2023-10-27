90d264be3cd5335a6268c7e387e6661e26476639
Ashihad <grzegorz.esser@gmail.com>
2023-10-27 14:15:56 +0200

    Fixed configPath initialization

--- src/ConfigParser.py
+++ src/ConfigParser.py
@@ -1,15 +1,13 @@
 from configparser import ConfigParser
 from pprint import pprint
 
-
 class ProgramConfig:
     """Class that stores global program configuration, read from a configFile file"""
     def __init__(self, configPath="config.ini"):
         self.configPath = configPath
-        self.config = None
-
-    def parse(self):
         self.config = ConfigParser()
+    
+    def parse(self):
         self.config.read(self.configPath)
         raw_dict = self.config._sections
         return raw_dict
